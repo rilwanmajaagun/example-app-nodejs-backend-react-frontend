9c522196a19fac135482cfe6342f962ad6f8f782
kollyd <majaagunoyinkolade@gmail.com>
2022-08-15 06:07:23 +0100

    add deploy to cloud run

--- src/client/components/ExampleComponent.js
+++ src/client/components/ExampleComponent.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function ExampleComponent() {
-	return <h1>Welcome to Majaagun first deployed Project</h1>;
+	return <div>
+		<h1>Welcome to Majaagun first deployed Project </h1>
+		<p>This project uses cloud run and github action for automatic deployment</p>
+		</div>;
 }
